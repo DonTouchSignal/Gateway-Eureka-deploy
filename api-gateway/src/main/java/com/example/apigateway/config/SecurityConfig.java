9fc2045da7bca72856bb33613f3d35b600e47f1c
Shining17 <bbvv777@naver.com>
2025-03-03 02:04:56 +0900

    ip 수정

--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -28,35 +28,35 @@ public class SecurityConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        
+
         // 구체적인 Origin 설정
-        config.addAllowedOrigin("http://34.222.150.136");
-        
+        config.addAllowedOrigin("http://54.69.96.52");
+
         // 모든 메서드 허용
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("DELETE");
         config.addAllowedMethod("OPTIONS");
-        
+
         // 필요한 모든 헤더 명시적 허용
         config.addAllowedHeader("Authorization");
         config.addAllowedHeader("Content-Type");
         config.addAllowedHeader("X-Auth-User");
         config.addAllowedHeader("Accept");
-        
+
         // 노출할 헤더 설정
         config.addExposedHeader("Authorization");
         config.addExposedHeader("X-Auth-User");
         config.addExposedHeader("AccessToken");
         config.addExposedHeader("RefreshToken");
-        
+
         // credentials 허용
         config.setAllowCredentials(true);
-        
+
         // preflight 요청의 캐시 시간 설정
         config.setMaxAge(3600L);
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsWebFilter(source);
@@ -67,10 +67,10 @@ public class SecurityConfig {
         return http
                 .cors(cors -> cors.configurationSource(source -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.addAllowedOrigin("http://34.222.150.136");
+                    config.addAllowedOrigin("http://54.69.96.52");
                     config.addAllowedMethod("*");
                     config.addAllowedHeader("*");
-                    
+
                     // WebSocket 관련 헤더 추가
                     config.addAllowedHeader("Upgrade");
                     config.addAllowedHeader("Connection");
@@ -78,7 +78,7 @@ public class SecurityConfig {
                     config.addAllowedHeader("Sec-WebSocket-Version");
                     config.addAllowedHeader("Sec-WebSocket-Extensions");
                     config.addAllowedHeader("Sec-WebSocket-Protocol");
-                    
+
                     config.addExposedHeader("Upgrade");
                     config.addExposedHeader("Connection");
                     config.addExposedHeader("Sec-WebSocket-Accept");
